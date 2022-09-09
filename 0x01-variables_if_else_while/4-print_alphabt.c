23f6a798fda6bf5ce705f8e6368686a9e365f11b
uthmanduro <udurosinlohun@gmail.com>
2022-09-09 16:38:10 -0700

    betty dey alright

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
- /**
-  * main - Entry point
-  *
-  * Return: Always 0(success)
-  */
+/**
+ * main - Entry point
+ *
+ * Return: always 0(success)
+ */
 int main(void)
 {
 	int i;
