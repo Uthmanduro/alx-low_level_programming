6c9c3136b64633fdbbdafedca6c99781d9e72413
uthmanduro <udurosinlohun@gmail.com>
2022-09-15 05:59:37 -0700

    positive debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - fuction that prints positive ornegative numbers
+ *
+ * Return: alwas 0
+ */
+void positive_or_negative(int i)
+{
+	int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+                printf("%d is positive\n", n);
+        else if (n == 0)
+                printf("%d is zero\n", n);
+        else if (n < 0)
+                printf("%d is negative\n", n);
+        return (0);
+}
+
