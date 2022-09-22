8395d577308aba06e4dea7c1b929afa111737ac0
uthmanduro <udurosinlohun@gmail.com>
2022-09-22 15:21:51 -0700

    trial

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_number - function that prints number to output
+ *
+ * @n: number to print
+ */
+void print_number(int n)
+{
+		_putchar(n + '0' );
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "unistd.h"
+/**
+ * _putchar - prints a character to output
+ * @c: character to be printed
+ */
+int _putchar(char c)
+{
+	return (write(1,&c, 1));
+}
