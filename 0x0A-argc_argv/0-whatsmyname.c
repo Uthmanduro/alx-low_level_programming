3e3e3a30f7077b025f48fbad6a0894cce17b142b
uthmanduro <udurosinlohun@gmail.com>
2022-09-30 16:40:05 -0700

    mynew name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
  * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	int i;
+
 	if (argc > 1)
 	{
 		for (i = 0; i < argc; i++)
-		{	
-		} 	
+		{}
 		printf("%s\n", argv[i]);
 	}
 	else
