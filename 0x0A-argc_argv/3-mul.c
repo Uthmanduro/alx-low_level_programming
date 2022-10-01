da2d12e49e66c591872e8d3c20221ed11cc74566
uthmanduro <udurosinlohun@gmail.com>
2022-09-30 17:47:16 -0700

    try mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: stgument count
+ * @argv: argument vctor
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc < 2)
+	printf("Error\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
