7d195d0726298bae228d4d24db33a62cffe2caf7
uthmanduro <udurosinlohun@gmail.com>
2022-10-10 15:34:24 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
+#ifndef "dog.h"
+#define "dog.h"
+/**
+ * struct dog - a strct
+ * @name: name of dog
+ * @age: the age
+ * @owner: the owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
