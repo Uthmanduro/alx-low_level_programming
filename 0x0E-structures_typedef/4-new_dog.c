65104e62f1508a73fbbb520bc287481abae13779
uthmanduro <udurosinlohun@gmail.com>
2022-10-11 17:05:30 -0700

    free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,13 +5,18 @@
  * @name: name
  * @age: age
  * @owner: owner
+ * Return: a pointer
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
