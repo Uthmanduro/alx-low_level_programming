3af3ce42122344ffd7f08a0c1dddb6fa3da59db7
uthmanduro <udurosinlohun@gmail.com>
2022-10-26 17:46:05 -0700

    inserts

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		if (temp == NULL)
-		       return (NULL);	
+			return (NULL);
 	}
 	ptr->next = temp->next;
 	temp->next = ptr;
