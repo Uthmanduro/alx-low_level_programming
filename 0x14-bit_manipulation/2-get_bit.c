f296253bc2b12ca076302673548e11cd11e85e31
uthmanduro <udurosinlohun@gmail.com>
2022-10-29 16:56:36 -0700

    get bit checking

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,10 +3,11 @@
  * get_bit - returns the value of a bit at a given index
  * @n: value to check
  * @index: index of the bit
+ * Return: an int
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if ((n >> index) == 0)
+	if ((n >> index) == 0 || index > 31)
 		return (-1);
 	return ((n >> index) & 1);
 }
