e3acc5fae95f096cbdc89a0a45efdbade1a53b70
uthmanduro <udurosinlohun@gmail.com>
2022-10-30 09:17:28 -0700

    clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,6 +9,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 31)
 		return (-1);
-	*n = ((*n >> (index + 1)) << (index + 1));
+	*n = *n & (~(1 << index));
 	return (1);
 }
