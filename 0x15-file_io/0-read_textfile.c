4a375c3c4d0fadcc33ac5c95f9707a4635a0c2c1
uthmanduro <udurosinlohun@gmail.com>
2022-11-02 12:07:36 -0700

    reads file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,21 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, rd, wr;
-	char *buf = malloc(letters);
+	char *buf;
 
+	buf = malloc(sizeof(char) * letters);
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDWR, 644);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	rd = read(fd, buf, letters);
 	if (rd == -1)
 		return (0);
-	wr = write(STDOUT_FILENO, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
 	if (wr == -1)
 		return (0);
 	close(fd);
-	return (rd);
+	free(buf);
+	return (wr);
 }
