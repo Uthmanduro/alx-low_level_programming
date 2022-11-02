18b336bd2d6ab2f11ed2808c089564aa3a12cc7f
uthmanduro <udurosinlohun@gmail.com>
2022-11-02 14:47:19 -0700

    acd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	count = strlen(text_content) + 1;
+	count = strlen(text_content);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
