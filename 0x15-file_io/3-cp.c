86b4bc1541a2381c987259e368cdf9f4660f2065
uthmanduro <udurosinlohun@gmail.com>
2022-11-03 18:20:24 -0700

    try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * closefile - close a file and print error if failed
+ * @fd: file decriptor to close
+ * Return: cl
+ */
+int closefile(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	return (cl);
+}
+/**
  * main - entry point
  * @argc: argument count
  * @argv: argument vector]
@@ -8,9 +25,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int fto, ffrom, wrto, rdfrom, clto, clfrom;
+	int fto, ffrom, wrto, rdfrom;
 	char *buf = malloc(1024);
 
+	if (!buf)
+	{
+		dprintf(STDERR_FILENO, "Cant write to %s\n", argv[2]);
+		exit(99);
+	}
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -23,19 +45,15 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664 & ~0000);
 	wrto = write(fto, buf, rdfrom);
 	if (fto == -1 || wrto == -1)
 	{
 		dprintf(STDERR_FILENO, "Cant write to %s\n", argv[2]);
 		exit(99);
 	}
-	clfrom = close(ffrom);
-	clto = close(fto);
-	if (clfrom == -1 || clto == -1)
-	{
-		dprintf(STDERR_FILENO, "Cant close fd %d\n", fto);
-		exit(100);
-	}
+	closefile(ffrom);
+	closefile(fto);
+	free(buf);
 	return (0);
 }
