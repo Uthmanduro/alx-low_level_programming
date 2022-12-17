794a90c765bed38654c805c61a422ad447e2bc2a
uthmanduro <udurosinlohun@gmail.com>
2022-12-17 11:30:21 -0800

    fee list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a new node at the end of a list
+ * @head: the head of the node
+ * @n: data in the node
+ * Return: address of a new element
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+	dlistint_t *ptr;
+
+	temp = malloc(sizeof(dlistint_t));
+	if (!temp)
+		return (NULL);
+	temp->prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+	ptr = *head;
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = temp;
+	temp->prev = *head;
+
+	return (temp);
+}
