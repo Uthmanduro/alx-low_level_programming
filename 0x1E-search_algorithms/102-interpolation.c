6feb10123c452859bb548146d226917f82744e40
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-04-16 19:39:05 -0700

    fixed bug

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,7 +12,7 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0, high, pos;
 
 	if (!array)
-                return (-1);
+		return (-1);
 	high = size - 1;
 	pos = low + (((double)(high - low) / (array[high] - array[low])) *
 				 (value - array[low]));
